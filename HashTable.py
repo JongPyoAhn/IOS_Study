21ed3d0f187fb788deecfd9ed573b6654ac367e5
JongPyoAhn <68585628+JongPyoAhn@users.noreply.github.com>
2021-06-23 11:13:27 +0900

    Enter

--- HashTable.py
+++ HashTable.py
@@ -105,7 +105,42 @@ def read_data(data):
     else:
         return None
 
-        
+#sha-256을 이용한 해시Chaining
+import hashlib
+hash_table = list([0 for i in range(8)])
+
+def get_key(data):
+    hash_object = hashlib.sha256()
+    hash_object.update(data.encode())
+    hex_dig = hash_object.hexdigest()
+    return int(hex_dig, 16)
+
+def hash_function(key):
+    return key % 8
+
+def save_data(data, value):
+    index_key = get_key(data)
+    hash_address = hash_function(index_key)
+    if hash_table[hash_address] != 0:
+        for index in range(len(hash_table[hash_address])):
+            if hash_table[hash_address][index][0] == index_key:
+                hash_table[hash_address][index][1] = value
+                return
+        hash_table[hash_address].append([index_key, value])
+    else:
+        hash_table[hash_address] = [[index_key, value]]
+    
+def read_data(data):
+    index_key = get_key(data)
+    hash_address = hash_function(index_key)
+
+    if hash_table[hash_address] != 0:
+        for index in range(len(hash_table[hash_address])):
+            if hash_table[hash_address][index][0] == index_key:
+                return hash_table[hash_address][index][1]
+        return None
+    else:
+        return None
 
 
 
