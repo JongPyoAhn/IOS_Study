0c306c2f4eb6aa752fa64ce5ef1d798dcd4a929c
JongPyoAhn <68585628+JongPyoAhn@users.noreply.github.com>
2021-07-04 09:50:19 +0900

    Add

--- 배열/배열/BOJ3052.swift
+++ 배열/배열/BOJ3052.swift
@@ -1,19 +1,19 @@
+////
+////  BOJ3052.swift
+////  배열
+////
+////  Created by 안종표 on 2021/07/02.
+////  Set을 쓸줄아는지 묻는 문제
 //
-//  BOJ3052.swift
-//  배열
+//import Foundation
 //
-//  Created by 안종표 on 2021/07/02.
-//  Set을 쓸줄아는지 묻는 문제
-
-import Foundation
-
-var s: Set<Int> = []
-var i = 0
-var count = 0
-while i < 10 {
-    s.insert(Int(readLine()!)! % 42)
-    i = i + 1
-}
-print(s.count)
-
-
+//var s: Set<Int> = []
+//var i = 0
+//var count = 0
+//while i < 10 {
+//    s.insert(Int(readLine()!)! % 42)
+//    i = i + 1
+//}
+//print(s.count)
+//
+//
