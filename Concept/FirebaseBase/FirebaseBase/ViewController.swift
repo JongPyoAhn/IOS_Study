e8b26eb34ad27af7fd9fb75f622ca523124e3dbe
JongPyoAhn <68585628+JongPyoAhn@users.noreply.github.com>
2021-07-20 10:39:23 +0900

    add

--- Concept/FirebaseBase/FirebaseBase/ViewController.swift
+++ Concept/FirebaseBase/FirebaseBase/ViewController.swift
@@ -7,18 +7,23 @@
 
 import UIKit
 import Firebase
+
 class ViewController: UIViewController {
 
     let db = Database.database().reference()
-    
+    var customers: [Customer] = []
     @IBOutlet weak var firstData: UILabel!
     
+    @IBOutlet weak var numberOfCustomers: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         updateLabel()
         variableTypeInsert()
-        savecustomers()
+        
+        
+//        updateBasicTypes()
+//        deleteBasicTypes()
     }
 
     func updateLabel(){
@@ -30,8 +35,77 @@ class ViewController: UIViewController {
             }
         }
     }
+    
+    @IBAction func createCustomer(_ sender: Any) {
+        savecustomers()
+    }
+    
+    @IBAction func readCustomer(_ sender: Any) {
+        fetchCustomers()
+    }
+    
+    @IBAction func updateCustomer(_ sender: Any) {
+        updateCustomer()
+    }
+    func updateCustomer(){
+        guard self.customers.isEmpty == false else {return}
+        customers[0].name = "min"
+        
+        let dictValue = customers.map{$0.toDictionary}
+        db.updateChildValues(["customers":dictValue])
+    }
+    @IBAction func deleteCustomer(_ sender: Any) {
+        deleteCustomer()
+    }
+    
+    func deleteCustomer(){
+        db.child("customers").removeValue()
+    }
+    
+    
+    
+}
+//데이터 수정 및 삭제
+extension ViewController {
+    func updateBasicTypes() {
+        db.updateChildValues(["int":6])
+        db.updateChildValues(["double":5.4])
+        db.updateChildValues(["str":"변경된String"])
+    }
+    func deleteBasicTypes() {
+        db.child("int").removeValue()
+        db.child("double").removeValue()
+        db.child("str").removeValue()
+       
+    }
 }
 
+
+// 데이터 fetch(불러오기)
+extension ViewController {
+    func fetchCustomers(){
+        db.child("customers").observeSingleEvent(of: .value){
+            snapshot in
+            print("--->\(snapshot)")
+            do{
+                let data = try JSONSerialization.data(withJSONObject: snapshot.value, options: [])
+                let decoder = JSONDecoder()
+                let customers: [Customer] = try decoder.decode([Customer].self, from: data)
+                self.customers = customers
+                DispatchQueue.main.async {
+                    self.numberOfCustomers.text = "\(customers.count)"
+                }
+                print("-----> \(customers.count)")
+            }catch let error{
+                print("\(error.localizedDescription)")
+            }
+        }
+    }
+}
+
+
+
+// 데이터 삽입
 extension ViewController {
     func variableTypeInsert(){
         db.child("int").setValue(3)
@@ -53,9 +127,9 @@ extension ViewController {
     }
 }
 
-struct Customer {
+struct Customer: Codable {
     let id: String
-    let name: String
+    var name: String
     let books: [Book]
     
     //오브젝트를 Firebase에 보내기위해서는 딕셔너리 형태로 변경해서 보내줘야한다.
@@ -66,7 +140,7 @@ struct Customer {
     }
     static var id = 0
 }
-struct Book {
+struct Book:Codable {
     let title: String
     let author: String
     
