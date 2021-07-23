16e0e810302562bdd40001d0d06c209be070e8ca
JongPyoAhn <68585628+JongPyoAhn@users.noreply.github.com>
2021-07-24 08:27:49 +0900

    Camera

---
+++ CameraMediaCapture/PreviewView.swift
@@ -0,0 +1,36 @@
+//
+//  PreviewView.swift
+//  CameraMediaCapture
+//
+//  Created by 안종표 on 2021/07/24.
+//
+
+import UIKit
+import AVFoundation
+
+class PreviewView: UIView {
+    var videoPreviewLayer: AVCaptureVideoPreviewLayer {
+        guard let layer = layer as? AVCaptureVideoPreviewLayer else {
+            fatalError("Expected `AVCaptureVideoPreviewLayer` type for layer. Check PreviewView.layerClass implementation.")
+        }
+        
+        layer.videoGravity = AVLayerVideoGravity.resizeAspectFill
+        layer.connection?.videoOrientation = .portrait
+        return layer
+    }
+    
+    var session: AVCaptureSession? {
+        get {
+            return videoPreviewLayer.session
+        }
+        set {
+            videoPreviewLayer.session = newValue
+        }
+    }
+    
+    // MARK: UIView
+    
+    override class var layerClass: AnyClass {
+        return AVCaptureVideoPreviewLayer.self
+    }
+}
