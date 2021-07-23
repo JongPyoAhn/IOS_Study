1c7a1f9c4a4929ef307d820b63e147b63a883b2a
JongPyoAhn <68585628+JongPyoAhn@users.noreply.github.com>
2021-07-24 07:22:11 +0900

    algorithm

---
+++ Algorithm/BOJ_Swift/Character/Character/BOJ_11720.swift
@@ -0,0 +1,19 @@
+//
+//  main.swift
+//  Character
+//
+//  Created by 안종표 on 2021/07/23.
+//
+//반복문이 어떻게 돌아가는지 아는지 묻는문제
+//var num: Int = Int(readLine()!)!
+//
+//var string = readLine()!
+//
+//var sum: Int = 0
+//
+//for i in string{
+//    sum = sum + Int(String(i))!
+//
+//}
+//
+//print(sum)
