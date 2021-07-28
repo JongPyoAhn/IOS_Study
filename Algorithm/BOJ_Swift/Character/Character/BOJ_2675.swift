75e0cf17fa11b5ba581324ad485845fc3fdd0229
JongPyoAhn <68585628+JongPyoAhn@users.noreply.github.com>
2021-07-28 10:06:10 +0900

    commit

---
+++ Algorithm/BOJ_Swift/Character/Character/BOJ_2675.swift
@@ -0,0 +1,19 @@
+//
+//  main.swift
+//  Character
+//
+//  Created by 안종표 on 2021/07/26.
+//
+//
+//let T = Int(readLine()!)!
+//for _ in 1...T{
+//    let arr = readLine()!.split(separator: " ")
+//    let R = Int(arr[0])!
+//    let S = arr[1]
+//    for j in S {
+//        for _ in 1...R {
+//            print("\(j)", terminator: "")
+//        }
+//    }
+//    print("")
+//}
