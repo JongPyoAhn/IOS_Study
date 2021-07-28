53aafb3414033b0c8c3abd22ea32039a1ec87b4c
JongPyoAhn <68585628+JongPyoAhn@users.noreply.github.com>
2021-07-28 21:01:24 +0900

    add Algorithm

---
+++ Algorithm/BOJ_Swift/Character/Character/BOJ_1152.swift
@@ -0,0 +1,26 @@
+//
+//  main.swift
+//  Character
+//
+//  Created by 안종표 on 2021/07/28.
+//
+
+//
+//let word = readLine()!
+//var arr: [Character] = []
+//var count = 0
+//for i in word {
+//    arr.append(i)
+//}
+//
+//for j in 0...(arr.count - 1) {
+//    if arr[j] == " " {
+//        count += 1
+//    }
+//}
+//
+//if arr[arr.count-1] == " " {
+//    count -= 1
+//}
+//
+//print("\(arr[0] == " " ? count : count + 1 )")
--- Algorithm/BOJ_Swift/Character/Character/main.swift
+++
@@ -1,8 +0,0 @@
-//
-//  main.swift
-//  Character
-//
-//  Created by 안종표 on 2021/07/28.
-//
-
-import Foundation
