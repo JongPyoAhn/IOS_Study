e28cb43bb490e2034bda7bc31006096583b32ceb
JongPyoAhn <68585628+JongPyoAhn@users.noreply.github.com>
2021-07-31 10:12:09 +0900

    add recursive

---
+++ Algorithm/BOJ_Swift/recursive_function/recursive_function/BOJ_10872.swift
@@ -0,0 +1,36 @@
+//
+//  main.swift
+//  recursive_function
+//
+//  Created by 안종표 on 2021/07/31.
+//
+// O
+//import Foundation
+// 그냥 푼거
+//let N = Int(readLine()!)!
+//var result = 1
+//factorial(N)
+//
+//func factorial(_ N:Int) {
+//    if N == 0 {
+//        print("\(result)")
+//        return
+//    }
+//    for i in 1...N {
+//        result = result * i
+//    }
+//    print("\(result)")
+//}
+
+
+// 공부하고 푼거
+//let N = Int(readLine()!)!
+//
+//print("\(factorial(N))")
+//
+//func factorial(_ N:Int) -> Int {
+//    if N == 0 {
+//        return 1
+//    }
+//    return N * factorial(N - 1)
+//}
