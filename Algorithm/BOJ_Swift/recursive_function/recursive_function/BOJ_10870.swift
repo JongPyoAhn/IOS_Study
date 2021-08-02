bd782f94898b6e790fae3f925a0c1b03d988bbff
JongPyoAhn <68585628+JongPyoAhn@users.noreply.github.com>
2021-08-02 09:41:32 +0900

    add BOJ

---
+++ Algorithm/BOJ_Swift/recursive_function/recursive_function/BOJ_10870.swift
@@ -0,0 +1,18 @@
+//
+//  main.swift
+//  recursive_function
+//
+//  Created by 안종표 on 2021/08/02.
+// O
+
+//
+//let n = Int(readLine()!)!
+//func pibo(_ n: Int) -> Int{
+//    if n == 0 {
+//        return 0
+//    }else if n == 1 {
+//        return 1
+//    }
+//    return pibo(n-1) + pibo(n-2)
+//}
+//print("\(pibo(n))")
