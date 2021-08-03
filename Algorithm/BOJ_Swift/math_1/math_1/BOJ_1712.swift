36fb3d841338f12f6bfb87377673983a91acc16d
JongPyoAhn <68585628+JongPyoAhn@users.noreply.github.com>
2021-08-03 22:02:08 +0900

    add BOJ

---
+++ Algorithm/BOJ_Swift/math_1/math_1/BOJ_1712.swift
@@ -0,0 +1,17 @@
+//
+//  main.swift
+//  math_1
+//
+//  Created by 안종표 on 2021/08/03.
+//
+
+//let n = (readLine()?.split(separator: " ").map{ Int($0)! })!
+//let A = n[0]
+//let B = n[1]
+//let C = n[2]
+//
+//if B >= C {
+//    print("-1")
+//}else{
+//    print("\((A / (C - B)) + 1)")
+//}
