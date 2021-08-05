62266d9c74ad08b5554450e2a08bf65088882535
JongPyoAhn <68585628+JongPyoAhn@users.noreply.github.com>
2021-08-05 23:26:11 +0900

    add BOJ

---
+++ Algorithm/BOJ_Swift/math_1/math_1/BOJ_10250.swift
@@ -0,0 +1,30 @@
+//
+//  main.swift
+//  math_1
+//
+//  Created by 안종표 on 2021/08/05.
+// O
+
+
+//let T = Int(readLine()!)!
+//for _ in 1...T {
+//    let arr = readLine()!.split(separator: " ").map{ Int($0)! }
+//    let H = arr[0]
+//    let W = arr[1]
+//    let N = arr[2]
+//    var count = 0
+//
+//    for i in 1...W {
+//        for j in 1...H {
+//            let Hosu = i + j * 100
+//            count += 1
+//            if count == N {
+//                print("\(Hosu)")
+//            }
+//        }
+//    }
+//}
+
+//for문으로 겉에서 호수, 속에서 층수
+// 101 ~ 601
+// 102 ~ 602
