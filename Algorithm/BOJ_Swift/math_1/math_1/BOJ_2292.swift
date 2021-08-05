9286f27c915407053bf32353b459c05592614aaf
JongPyoAhn <68585628+JongPyoAhn@users.noreply.github.com>
2021-08-05 13:43:08 +0900

    add BOJ

---
+++ Algorithm/BOJ_Swift/math_1/math_1/BOJ_2292.swift
@@ -0,0 +1,23 @@
+//
+//  main.swift
+//  math_1
+//
+//  Created by 안종표 on 2021/08/03.
+// O
+
+//
+//let n = Int(readLine()!)!
+//var sum = 1
+//var count = 1
+//for i in 1...n {
+//    if n == 1 {
+//        print("\(count)")
+//        break
+//    }
+//    if n <= sum {
+//        print("\(count)")
+//        break
+//    }
+//    sum += 6 * i
+//    count += 1
+//}
