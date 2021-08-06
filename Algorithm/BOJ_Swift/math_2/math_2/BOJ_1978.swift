5cde110fd7479737bb9c7639ae42f94f35b13755
JongPyoAhn <68585628+JongPyoAhn@users.noreply.github.com>
2021-08-06 20:31:56 +0900

    add BOJ

---
+++ Algorithm/BOJ_Swift/math_2/math_2/BOJ_1978.swift
@@ -0,0 +1,29 @@
+//
+//  main.swift
+//  math_2
+//
+//  Created by 안종표 on 2021/08/06.
+// O
+
+
+//
+//let N = Int(readLine()!)!
+//var count = 0
+//var isPrimeNumber = true
+//var arr = readLine()!.split(separator: " ").map{ Int($0)! }
+//for i in 0..<arr.count {
+//    if arr[i] == 1 {
+//        isPrimeNumber = false
+//    }else {
+//        for j in 2..<arr[i] {
+//            if arr[i] % j == 0 {
+//                isPrimeNumber = false
+//            }
+//        }
+//    }
+//    if isPrimeNumber == true {
+//        count += 1
+//    }
+//    isPrimeNumber = true
+//}
+//print("\(count)")
