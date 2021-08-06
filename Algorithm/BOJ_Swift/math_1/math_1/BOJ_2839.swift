c5f585169a520926b93afab018596c9aa02b5c0b
JongPyoAhn <68585628+JongPyoAhn@users.noreply.github.com>
2021-08-06 19:50:49 +0900

    add BOJ

---
+++ Algorithm/BOJ_Swift/math_1/math_1/BOJ_2839.swift
@@ -0,0 +1,29 @@
+//
+//  main.swift
+//  math_1
+//
+//  Created by 안종표 on 2021/08/06.
+//
+
+//
+//var N = Int(readLine()!)!
+//var result = 0
+//if N % 5 == 0 {
+//    result = N / 5
+//    N = 0
+//}else {
+//    while N > 0 {
+//        N -= 3
+//        result += 1
+//        if N % 5 == 0 {
+//            result += N / 5
+//            N = N % 5
+//            break
+//        }
+//    }
+//}
+//
+//if N != 0 {
+//    result = -1
+//}
+//print("\(result)")
