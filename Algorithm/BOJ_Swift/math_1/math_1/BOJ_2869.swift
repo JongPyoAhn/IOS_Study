217ae2ebf5c74d155c5219ab0bd052a8d616f716
JongPyoAhn <68585628+JongPyoAhn@users.noreply.github.com>
2021-08-06 13:27:37 +0900

    add BOJ

---
+++ Algorithm/BOJ_Swift/math_1/math_1/BOJ_2869.swift
@@ -0,0 +1,18 @@
+//
+//  main.swift
+//  math_1
+//
+//  Created by 안종표 on 2021/08/06.
+// O
+//import Foundation
+//let arr = readLine()!.split(separator: " ").map{Double($0)!}
+//let A = arr[0]
+//let B = arr[1]
+//let V = arr[2]
+//
+//var x = ceil((V - B) / (A - B))
+//print("\(Int(x))")
+
+//숏코딩
+//import Foundation
+//let a = readLine()!.split(separator: " ").map{Double($0)!};print("\(Int(ceil((a[2] - a[1]) / (a[0] - a[1]))))")
