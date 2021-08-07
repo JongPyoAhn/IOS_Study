05fb8f1fd6dd4e2b28870435223fe12583a3fcd9
JongPyoAhn <68585628+JongPyoAhn@users.noreply.github.com>
2021-08-07 14:49:28 +0900

    add BOJ

---
+++ Algorithm/BOJ_Swift/math_2/math_2/BOJ_2581.swift
@@ -0,0 +1,44 @@
+//
+//  main.swift
+//  math_2
+//
+//  Created by 안종표 on 2021/08/07.
+// O
+
+//
+//let M = Int(readLine()!)!
+//let N = Int(readLine()!)!
+//
+//var sum = 0
+//var isPrimeNumber = true
+//var allPrimeNumber = false
+//var arr: [Int] = []
+//
+//for i in M...N {
+//    if i == 1 {
+//        isPrimeNumber = false
+//    }else {
+//        for j in 2..<i {
+//            if i % j == 0 {
+//                isPrimeNumber = false
+//            }
+//        }
+//
+//        if isPrimeNumber == true {
+//            arr.append(i)
+//            allPrimeNumber = true
+//        }
+//    }
+//    isPrimeNumber = true
+//}
+//
+//if allPrimeNumber {
+//    for k in 0..<arr.count {
+//        sum += arr[k]
+//    }
+//    print("\(sum)")
+//    print("\(arr.min()!)")
+//}else {
+//    print("-1")
+//}
+
