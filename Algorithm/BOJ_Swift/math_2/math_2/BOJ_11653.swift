fab10b002cefbf1c4235529feae4ddc684673224
JongPyoAhn <68585628+JongPyoAhn@users.noreply.github.com>
2021-08-07 19:24:48 +0900

    add BOJ

---
+++ Algorithm/BOJ_Swift/math_2/math_2/BOJ_11653.swift
@@ -0,0 +1,17 @@
+//
+//  main.swift
+//  math_2
+//
+//  Created by 안종표 on 2021/08/07.
+//
+
+//var N = Int(String(readLine()!))!
+//if N == 1 {
+//    print("")
+//}
+//for i in stride(from: 2, through: N, by: 1) {
+//    while N % i == 0 {
+//        print("\(i)")
+//        N = N / i
+//    }
+//}
