d13a0b970c8863e31b8370edd413df16768ad8b8
JongPyoAhn <68585628+JongPyoAhn@users.noreply.github.com>
2021-08-08 18:13:30 +0900

    add BOJ

---
+++ Algorithm/BOJ_Swift/math_2/math_2/Eratosthenes' sift.swift
@@ -0,0 +1,25 @@
+//
+//  main.swift
+//  math_2
+//
+//  Created by 안종표 on 2021/08/08.
+// 연습하기
+
+//import Foundation
+//let number = 25
+//var arr: [Int] = Array(repeating: 0, count: number + 1)
+//
+//for i in 2...number {
+//    arr[i] = i
+//}
+//for j in 2...number {
+//    if arr[j] == 0 { continue }
+//    for k in stride(from: j + j, through: number, by: j) {
+//        arr[k] = 0
+//    }
+//}
+//for w in 2..<number {
+//    if arr[w] != 0 {
+//        print("\(arr[w])")
+//    }
+//}
