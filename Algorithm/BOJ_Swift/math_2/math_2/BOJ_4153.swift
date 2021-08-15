02f7b9f76ff7bd86bec83b46cf77dac202cb2703
JongPyoAhn <68585628+JongPyoAhn@users.noreply.github.com>
2021-08-15 11:19:51 +0900

    add BOJ

---
+++ Algorithm/BOJ_Swift/math_2/math_2/BOJ_4153.swift
@@ -0,0 +1,32 @@
+//
+//  main.swift
+//  math_2
+//
+//  Created by 안종표 on 2021/08/15.
+// O
+
+
+//while true {
+//    var jegop = 0
+//    var i = 0
+//    let arr = readLine()!.split(separator: " ").map{ Int($0)! }
+//
+//    if arr == [0, 0, 0] {
+//        break
+//    }
+//
+//    while i < arr.count {
+//        if i != arr.firstIndex(of: arr.max()!) {
+//            jegop += arr[i] * arr[i]
+//        }
+//        i += 1
+//    }
+//
+//
+//    if jegop == (arr.max()! * arr.max()!) {
+//        print("right")
+//    }else {
+//        print("wrong")
+//    }
+//
+//}
