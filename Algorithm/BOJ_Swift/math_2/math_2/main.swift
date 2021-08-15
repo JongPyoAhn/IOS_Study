1baeb965ed0bf64d544c0998cf22e1adbd04a124
JongPyoAhn <68585628+JongPyoAhn@users.noreply.github.com>
2021-08-15 09:25:40 +0900

    add BOJ

---
+++ Algorithm/BOJ_Swift/math_2/math_2/main.swift
@@ -0,0 +1,17 @@
+//
+//  main.swift
+//  math_2
+//
+//  Created by 안종표 on 2021/08/15.
+//
+
+var arr = readLine()!.split(separator: " ").map{Int($0)!}
+arr[2] = arr[2] - arr[0]
+arr[3] = arr[3] - arr[1]
+var temp = arr[0]
+for i in 0..<arr.count {
+    if temp > arr[i] {
+        temp = arr[i]
+    }
+}
+print("\(temp)")
