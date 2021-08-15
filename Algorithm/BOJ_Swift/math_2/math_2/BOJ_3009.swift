26adbcfa2ca0c19b5927375f0f2782fe5f59cb37
JongPyoAhn <68585628+JongPyoAhn@users.noreply.github.com>
2021-08-15 10:21:43 +0900

    add BOJ

---
+++ Algorithm/BOJ_Swift/math_2/math_2/BOJ_3009.swift
@@ -0,0 +1,22 @@
+//
+//  main.swift
+//  math_2
+//
+//  Created by 안종표 on 2021/08/15.
+// O
+
+//let arr1 = readLine()!.split(separator: " ").map{Int($0)!}
+//let arr2 = readLine()!.split(separator: " ").map{Int($0)!}
+//let arr3 = readLine()!.split(separator: " ").map{Int($0)!}
+//var arr4:[Int] = []
+//var temp = 0
+//for i in 0..<arr1.count {
+//    if arr1[i] == arr2[i] {
+//        arr4.append(arr3[i])
+//    }else if arr2[i] == arr3[i]  {
+//        arr4.append(arr1[i])
+//    }else if arr1[i] == arr3[i] {
+//        arr4.append(arr2[i])
+//    }
+//}
+//print("\(arr4[0]) \(arr4[1])")
