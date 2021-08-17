dc825396f7b53599c22db366b3627eeb870c9e03
JongPyoAhn <68585628+JongPyoAhn@users.noreply.github.com>
2021-08-17 19:07:41 +0900

    add BOJ

---
+++ Algorithm/BOJ_Swift/recursive_function/recursive_function/main.swift
@@ -0,0 +1,23 @@
+//
+//  main.swift
+//  recursive_function
+//
+//  Created by 안종표 on 2021/08/16.
+//
+
+//import Foundation
+//let n = Int(readLine()!)!
+//print("\(pow(2, n) - 1)")
+//hanoi(n, 1, 2, 3)
+//
+//
+//func hanoi(_ n: Int, _ start: Int, _ mid: Int, _ end: Int) {
+//    if n == 1 {
+//        print("\(start) \(end)")
+//        return
+//    }
+//    hanoi(n-1, start, end, mid)
+//    hanoi(1, start, mid, end)
+//    hanoi(n-1, mid, start, end)
+//}
+
