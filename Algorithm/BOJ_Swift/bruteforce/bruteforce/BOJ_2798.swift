96d696688d240128bb77fe53d163da84d74e851e
JongPyoAhn <68585628+JongPyoAhn@users.noreply.github.com>
2021-08-18 11:47:15 +0900

    add BOJ

---
+++ Algorithm/BOJ_Swift/bruteforce/bruteforce/BOJ_2798.swift
@@ -0,0 +1,28 @@
+//
+//  main.swift
+//  bruteforce
+//
+//  Created by 안종표 on 2021/08/18.
+// O
+
+//let arr = readLine()!.split(separator: " ").map{ Int($0)! }
+//let n = arr[0]
+//let m = arr[1]
+//var suArr = readLine()!.split(separator: " ").map{Int($0)!}
+//var sum = 0
+//var temp = 0
+//
+//for i in 0..<n {
+//    for j in i+1..<n {
+//        for k in j+1..<n {
+//            if suArr[i] + suArr[j] + suArr[k] <= m {
+//                if suArr[i] + suArr[j] + suArr[k] > sum {
+//                    sum = suArr[i] + suArr[j] + suArr[k]
+//                }
+//
+//            }
+//        }
+//    }
+//
+//}
+//print("\(sum)")
