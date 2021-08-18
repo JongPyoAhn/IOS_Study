0eb13fe3322121793671c7d2ab24b4cdb93c1fda
JongPyoAhn <68585628+JongPyoAhn@users.noreply.github.com>
2021-08-18 13:05:48 +0900

    add BOJ

---
+++ Algorithm/BOJ_Swift/bruteforce/bruteforce/BOJ_2231.swift
@@ -0,0 +1,26 @@
+//
+//  main.swift
+//  bruteforce
+//
+//  Created by 안종표 on 2021/08/18.
+// O
+
+//let n = Int(readLine()!)!
+//var sum = 0
+//var realI = 0
+//
+//for i in 1...n {
+//    sum = i
+//    for j in String(i) {
+//        sum += Int(String(j))!
+//    }
+//    if sum == n {
+//        realI = i
+//        break
+//    }
+//}
+//if realI == 0 {
+//    print("0")
+//}else {
+//    print("\(realI)")
+//}
