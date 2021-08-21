4fe8d08243400069017c864300277debd40a1732
JongPyoAhn <68585628+JongPyoAhn@users.noreply.github.com>
2021-08-21 15:48:12 +0900

    add BOJ

---
+++ Algorithm/BOJ_Swift/ArrayLineup/ArrayLineup/BOJ_1427.swift
@@ -0,0 +1,18 @@
+//
+//  main.swift
+//  ArrayLineup
+//
+//  Created by 안종표 on 2021/08/21.
+// O
+
+//let n = readLine()!
+//var arr: [Int] = []
+//
+//for i in n {
+//    arr.append(Int(String(i))!)
+//}
+//arr.sort(by: >)
+//
+//for k in 0..<arr.count {
+//    print("\(arr[k])", terminator: "")
+//}
