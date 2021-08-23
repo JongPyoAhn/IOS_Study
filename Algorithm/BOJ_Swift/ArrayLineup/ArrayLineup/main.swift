8f3cf6dda574be0b36ac90c5f5ab4b56108924db
JongPyoAhn <68585628+JongPyoAhn@users.noreply.github.com>
2021-08-23 16:59:39 +0900

    add BOJ

---
+++ Algorithm/BOJ_Swift/ArrayLineup/ArrayLineup/main.swift
@@ -0,0 +1,22 @@
+//
+//  main.swift
+//  ArrayLineup
+//
+//  Created by 안종표 on 2021/08/23.
+// O
+
+//let n = Int(readLine()!)!
+//var dict =  [String : Int]()
+//
+//for _ in 0..<n {
+//    let word = readLine()!
+//    dict[word, default: 0] += 1
+//}
+//
+//var sortedDict = dict.sorted {
+//    $0.key.count == $1.key.count ? $0 < $1 : $0.key.count < $1.key.count
+//}
+//
+//for i in 0..<sortedDict.count {
+//    print("\(sortedDict[i].key)")
+//}
