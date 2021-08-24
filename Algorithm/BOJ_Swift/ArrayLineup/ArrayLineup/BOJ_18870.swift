074202a22d08428e275bf5a635dcc7b812d9447c
JongPyoAhn <68585628+JongPyoAhn@users.noreply.github.com>
2021-08-24 13:38:00 +0900

    add BOJ

---
+++ Algorithm/BOJ_Swift/ArrayLineup/ArrayLineup/BOJ_18870.swift
@@ -0,0 +1,19 @@
+//
+//  main.swift
+//  ArrayLineup
+//
+//  Created by 안종표 on 2021/08/23.
+//
+
+//let n = Int(readLine()!)!
+//let arr = readLine()!.split(separator: " ").map{Int(String($0))!}
+//var dict = [Int:Int]()
+//var next = 0
+//for i in arr.sorted() {
+//    if dict[i] == nil {
+//        dict[i] = next
+//        next += 1
+//    }
+//}
+//print("\(arr.map{ String(dict[$0]!) }.joined(separator: " "))")
+
