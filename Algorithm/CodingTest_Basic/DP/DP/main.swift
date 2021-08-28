41cf509d951ed3f28e7c7fb89604ca0b6426c14c
JongPyoAhn <68585628+JongPyoAhn@users.noreply.github.com>
2021-08-28 14:14:27 +0900

    add BOJ

--- Algorithm/CodingTest_Basic/DP/DP/main.swift
+++ Algorithm/CodingTest_Basic/DP/DP/main.swift
@@ -5,7 +5,22 @@
 //  Created by 안종표 on 2021/08/28.
 //
 
-import Foundation
-
-print("Hello, World!")
-
+//var n = Int(readLine()!)!
+//var dp = Array(repeating: 0, count: 1000001) //몇번 움직였는지의 결과를 저장하기위한 배열
+//
+//if n == 1 {
+//    print("0")
+//}else {
+//    for i in 2...n {
+//        dp[i] = dp[i - 1] + 1
+//
+//        if i % 2 == 0 {
+//            dp[i] = dp[i] >= dp[i / 2] + 1 ? dp[i / 2] + 1 : dp[i]
+//        }
+//        if i % 3 == 0 {
+//            dp[i] = dp[i] >= dp[i / 3] + 1 ? dp[i / 3] + 1 : dp[i]
+//        }
+//
+//    }
+//    print("\(dp[n])")
+//}
