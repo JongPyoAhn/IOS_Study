c294ae276555dbc482de18729b9315da6a71aa35
JongPyoAhn <68585628+JongPyoAhn@users.noreply.github.com>
2021-08-31 14:22:05 +0900

    add BOJ

---
+++ Algorithm/CodingTest_Basic/greedy/greedy/BOJ_5585.swift
@@ -0,0 +1,34 @@
+//
+//  main.swift
+//  greedy
+//
+//  Created by 안종표 on 2021/08/31.
+// O
+
+//let n = Int(readLine()!)!
+//var money = 1000 - n
+//var count = 0
+//while true{
+//    if money == 0 {
+//        break
+//    }
+//
+//    if money >= 500 {
+//        money -= 500
+//    }else if money >= 100 {
+//        money -= 100
+//    }else if money >= 50 {
+//        money -= 50
+//    }else if money >= 10 {
+//        money -= 10
+//    }else if money >= 5 {
+//        money -= 5
+//    }else {
+//        money -= 1
+//    }
+//    count += 1
+//}
+//print("\(count)")
+
+
+
