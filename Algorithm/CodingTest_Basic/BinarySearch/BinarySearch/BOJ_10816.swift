33b53082c4729c5a4960346519d83f22414c477b
JongPyoAhn <68585628+JongPyoAhn@users.noreply.github.com>
2021-09-03 10:25:43 +0900

    add ThisCoding

---
+++ Algorithm/CodingTest_Basic/BinarySearch/BinarySearch/BOJ_10816.swift
@@ -0,0 +1,35 @@
+//
+//  main.swift
+//  BinarySearch
+//
+//  Created by 안종표 on 2021/09/02.
+//
+
+//let n = Int(readLine()!)!
+//var nArr = readLine()!.split(separator: " ").map{Int(String($0))!}
+//
+//let m = Int(readLine()!)
+//let mArr = readLine()!.split(separator: " ").map{Int(String($0))!}
+//
+//var dict = [Int: Int]()
+//var result: [Int] = []
+//
+//nArr.sort()
+//
+//for i in nArr{
+//    if dict.keys.contains(i) {
+//        dict[i]! += 1
+//    }else {
+//        dict[i] = 1
+//    }
+//}
+//
+//for j in mArr{
+//    if dict.keys.contains(j) {
+//        result.append(dict[j]!)
+//    }else {
+//        result.append(0)
+//    }
+//}
+//
+//print(result.map{String($0)}.joined(separator: " "))
