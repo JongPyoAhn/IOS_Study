1f5c0d285d88e8358800df87f465fe025c4a5514
JongPyoAhn <68585628+JongPyoAhn@users.noreply.github.com>
2021-09-04 22:24:28 +0900

    add Review

---
+++ Algorithm/CodingTest_Basic/Review/Review/BOJ_10816(Binary).swift
@@ -0,0 +1,32 @@
+//
+//  main.swift
+//  Review
+//
+//  Created by 안종표 on 2021/09/04.
+// X
+//
+//let n = Int(readLine()!)!
+//var nArr = readLine()!.split(separator: " ").map{Int($0)!}
+//let m = Int(readLine()!)!
+//let mArr = readLine()!.split(separator: " ").map{Int($0)!}
+//var dict = [Int:Int]()
+//var result: [Int] = []
+//
+//nArr.sort()
+//for i in nArr{
+//    if dict.keys.contains(i){
+//        dict[i]! += 1
+//    }else {
+//        dict[i] = 1
+//    }
+//}
+//
+//for j in mArr{
+//    if dict.keys.contains(j){
+//        result.append(dict[j]!)
+//    }else {
+//        result.append(0)
+//    }
+//}
+//
+//print(result.map{String($0)}.joined(separator: " "))
