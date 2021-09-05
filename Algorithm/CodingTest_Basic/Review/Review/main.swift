65c80f3b9ed9400a21be3a73f3115f8580b5fe86
JongPyoAhn <68585628+JongPyoAhn@users.noreply.github.com>
2021-09-05 09:25:16 +0900

    add Review

---
+++ Algorithm/CodingTest_Basic/Review/Review/main.swift
@@ -0,0 +1,25 @@
+//
+//  main.swift
+//  Review
+//
+//  Created by 안종표 on 2021/09/05.
+// X
+
+
+//while let n = readLine(){
+//    var count = 0
+//    var next = 1
+//    while true{
+//        count += 1
+//        if next % Int(n)! == 0 {
+//            print("\(count)")
+//            break
+//        }else{
+//            next = next * 10 + 1
+//            next %= Int(n)!
+//
+//        }
+//    }
+//
+//}
+
