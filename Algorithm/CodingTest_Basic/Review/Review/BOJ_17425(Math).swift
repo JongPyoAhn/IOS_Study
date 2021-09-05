fa4b86ba1d28433662f3c8bf4ad1d3cd360ac53a
JongPyoAhn <68585628+JongPyoAhn@users.noreply.github.com>
2021-09-05 12:12:14 +0900

    add Review

---
+++ Algorithm/CodingTest_Basic/Review/Review/BOJ_17425(Math).swift
@@ -0,0 +1,26 @@
+//
+//  main.swift
+//  Review
+//
+//  Created by 안종표 on 2021/09/05.
+// X
+
+//import Foundation
+//var dp = Array(repeating: 0, count: 1000001)
+//var s = Array(repeating: 1, count: 1000001)
+//dp[1] = 1
+//for i in stride(from: 2, through: 1000000, by: 1){
+//    var j = 1
+//    while j<=1000000 {
+//        dp[i*j] += i
+//        j += 1
+//    }
+//}
+//for j in 2...1000000{
+//    s[j] = s[j - 1] + dp[j]
+//}
+//let T = Int(readLine()!)!
+//for _ in 1...T{
+//    let n = Int(readLine()!)!
+//    print("\(dp[n])")
+//}
--- Algorithm/CodingTest_Basic/Review/Review/BOJ_17427(Math).swift
+++ Algorithm/CodingTest_Basic/Review/Review/BOJ_17427(Math).swift
@@ -5,18 +5,18 @@
 //  Created by 안종표 on 2021/09/05.
 // X
 
-let n = Int(readLine()!)!
-var arr = Array(repeating: 0, count: n + 1)
-
-for i in 1...n{
-    arr[i] = i
-}
-var sum = 0
-for i in stride(from: 2, through: n, by: 1){
-    for j in 1...n{
-        if arr[i] % j == 0{
-            sum += j
-        }
-    }
-}
-print("\(sum + 1)")
+//let n = Int(readLine()!)!
+//var arr = Array(repeating: 0, count: n + 1)
+//
+//for i in 1...n{
+//    arr[i] = i
+//}
+//var sum = 0
+//for i in stride(from: 2, through: n, by: 1){
+//    for j in 1...n{
+//        if arr[i] % j == 0{
+//            sum += j
+//        }
+//    }
+//}
+//print("\(sum + 1)")
