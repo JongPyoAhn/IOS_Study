00189465afd7d583bd173e63ccd8f89bd0807328
JongPyoAhn <68585628+JongPyoAhn@users.noreply.github.com>
2021-09-05 19:52:57 +0900

    add Review

---
+++ Algorithm/CodingTest_Basic/Review/Review/BOJ_11727(DP).swift
@@ -0,0 +1,17 @@
+//
+//  main.swift
+//  Review
+//
+//  Created by 안종표 on 2021/09/05.
+// X
+
+//let n = Int(readLine()!)!
+//var arr = Array(repeating: 0, count: 1001)
+//
+//arr[1] = 1
+//arr[2] = 3
+//
+//for i in stride(from: 3, through: n, by: 1) {
+//    arr[i] = (arr[i - 1] + arr[i - 2] + arr[i - 2]) % 10007
+//}
+//print("\(arr[n])")
