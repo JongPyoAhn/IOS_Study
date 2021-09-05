b4cd8d19409d3128ea439558cf1d1b619c46b51d
JongPyoAhn <68585628+JongPyoAhn@users.noreply.github.com>
2021-09-05 18:01:55 +0900

    add Review

---
+++ Algorithm/CodingTest_Basic/Review/Review/BOJ_1463(DP).swift
@@ -0,0 +1,22 @@
+//
+//  main.swift
+//  Review
+//
+//  Created by 안종표 on 2021/09/05.
+// X
+
+//let n = Int(readLine()!)!
+//var dp = Array(repeating: 0, count: 1000001)
+//dp[2] = 1
+//dp[3] = 1
+//for i in stride(from: 4, through: n, by: 1){
+//    dp[i] = dp[i - 1] + 1
+//    if i % 2 == 0 {
+//        dp[i] = dp[i / 2] + 1 <= dp[i] ? dp[i / 2] + 1 : dp[i]
+//    }
+//    if i % 3 == 0{
+//        dp[i] = dp[i / 3] + 1 <= dp[i] ? dp[i / 3] + 1 : dp[i]
+//    }
+//}
+//print("\(dp[n])")
+//
---
+++ Algorithm/CodingTest_Basic/Review/Review/main.swift
@@ -0,0 +1,8 @@
+//
+//  main.swift
+//  Review
+//
+//  Created by 안종표 on 2021/09/05.
+//
+
+import Foundation
