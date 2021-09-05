0bbc86af989efbe994dad9d9d3d4425467daeca0
JongPyoAhn <68585628+JongPyoAhn@users.noreply.github.com>
2021-09-05 12:30:43 +0900

    add Review

---
+++ Algorithm/CodingTest_Basic/Review/Review/BOJ_2609(Math).swift
@@ -0,0 +1,18 @@
+//
+//  main.swift
+//  Review
+//
+//  Created by 안종표 on 2021/09/05.
+// X
+
+//let arr = readLine()!.split(separator: " ").map{Int($0)!}
+//var n = arr.max()!
+//var m = arr.min()!
+//
+//while n % m != 0 {
+//    let temp = m
+//    m = n % m
+//    n = temp
+//}
+//print(m)
+//print("\( (arr[0] / m) * (arr[1] / m) * m  )")
