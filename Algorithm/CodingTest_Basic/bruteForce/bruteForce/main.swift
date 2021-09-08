ac68f899cec64170526b556414d649443673162c
JongPyoAhn <68585628+JongPyoAhn@users.noreply.github.com>
2021-09-08 23:20:51 +0900

    add BOJ

--- Algorithm/CodingTest_Basic/bruteForce/bruteForce/main.swift
+++ Algorithm/CodingTest_Basic/bruteForce/bruteForce/main.swift
@@ -2,7 +2,94 @@
 //  main.swift
 //  bruteForce
 //
-//  Created by 안종표 on 2021/09/07.
+//  Created by 안종표 on 2021/08/26.
+// O
+
+//let n = Int(readLine()!)!
+//var arr: [[Character]] = Array(repeating: Array(repeating: "A", count: n), count: n)
+//var result = 0
+//var realResult = 0
+//for i in 0..<n {
+//    arr[i] = readLine()!.map{Character(String($0))}
+//}
 //
-
-
+//for i in 0..<n { //가로 바꾸기
+//    for j in 0..<n{
+//        if j + 1 < n, arr[i][j] != arr[i][j + 1] {
+//            swap2DArray(i, j)
+//            let a = 가로()
+//            let b = 세로()
+//            result = max(a, b)
+//
+//            swap2DArray(i, j)
+//
+//        }
+//        realResult = max(result, realResult)
+//    }
+//}
+//
+//for i in 0..<n { //세로 바꾸기
+//    for j in 0..<n{
+//        if j + 1 < n, arr[j][i] != arr[j + 1][i] {
+//            swap2DArray세로(j, i) //arr.swapAt([j][i], [j][i + 1])
+//            let a = 가로()
+//            let b = 세로()
+//            result = max(a, b)
+//            swap2DArray세로(j, i) //arr.swapAt([j][i], [j][i + 1])
+//        }
+//        realResult = max(result, realResult)
+//    }
+//}
+//
+//print("\(realResult)")
+//
+//func 가로() -> Int{ //가로 비교
+//
+//    var prev = 0
+//    for k in 0..<n {
+//        var sum = 1
+//        for v in 0..<n {
+//
+//            if v + 1 < n, arr[k][v] == arr[k][v + 1] {
+//                sum += 1
+//            }else if v + 1 < n, arr[k][v] != arr[k][v + 1]{
+//                prev = max(prev, sum) //실수한 부분
+//                sum = 1
+//            }
+//        }
+//        prev = max(prev, sum)
+//    }
+//    return prev
+//}
+//
+//func 세로() -> Int{ //세로 비교
+//
+//    var prev = 0
+//    for k in 0..<n {
+//        var sum = 1
+//        for v in 0..<n {
+//            if v + 1 < n, arr[v][k] == arr[v + 1][k] {
+//                sum += 1
+//            }else if v + 1 < n, arr[v][k] != arr[v + 1][k]{
+//                prev = max(prev, sum) //실수한 부분
+//                sum = 1
+//            }
+//
+//        }
+//
+//        prev = max(prev, sum)
+//    }
+//    return prev
+//}
+//
+//
+//func swap2DArray(_ a: Int, _ b: Int){
+//    let temp = arr[a][b]
+//    arr[a][b] = arr[a][b + 1]
+//    arr[a][b + 1] = temp
+//}
+//func swap2DArray세로(_ a: Int, _ b: Int){
+//    let temp = arr[a][b]
+//    arr[a][b] = arr[a + 1][b]
+//    arr[a + 1][b] = temp
+//}
