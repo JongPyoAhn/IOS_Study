5a92c35e306f0a6147d13f0dcc761f4451c86523
JongPyoAhn <68585628+JongPyoAhn@users.noreply.github.com>
2021-09-09 19:16:39 +0900

    add BOJ

---
+++ Algorithm/CodingTest_Basic/DFS&BFS/DFS&BFS/BOJ_10828(스택).swift
@@ -0,0 +1,65 @@
+//
+//  main.swift
+//  DFS&BFS
+//
+//  Created by 안종표 on 2021/09/09.
+//
+
+
+//var stack:[Int] = []
+//let n = Int(readLine()!)!
+//for _ in 1...n {
+//    let a = readLine()!.split(separator: " ").map{String($0)}
+//    switch a[0] {
+//    case "push":
+//        push(Int(a[1])!)
+//        break
+//    case "pop":
+//        print(pop())
+//        break
+//    case "size":
+//        print(size())
+//        break
+//    case "empty":
+//        print(empty())
+//        break
+//    case "top":
+//        print(top())
+//        break
+//    default:
+//        break
+//    }
+//}
+//
+//
+//func push(_ x: Int){
+//    stack.append(x)
+//}
+//
+//func pop() -> Int{
+//    if let pop = stack.popLast() {
+//        return pop
+//    }else {
+//       return -1
+//    }
+//}
+//
+//func size() ->Int{
+//    return stack.count
+//}
+//
+//func empty() -> Int{
+//    if stack.isEmpty {
+//        return 1
+//    }else {
+//        return 0
+//    }
+//}
+//
+//func top() -> Int{
+//    if let last = stack.last {
+//        return last
+//    }else {
+//        return -1
+//    }
+//}
