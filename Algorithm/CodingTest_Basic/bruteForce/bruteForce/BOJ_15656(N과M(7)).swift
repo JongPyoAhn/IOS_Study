f87ab68170b06ec5ab0ecfba86b2ac4104f743c5
JongPyoAhn <68585628+JongPyoAhn@users.noreply.github.com>
2021-09-30 14:38:16 +0900

    add BOJ

---
+++ Algorithm/CodingTest_Basic/bruteForce/bruteForce/BOJ_15656(N과M(7)).swift
@@ -0,0 +1,32 @@
+//
+//  main.swift
+//  bruteForce
+//
+//  Created by 안종표 on 2021/09/30.
+//
+
+//let nm = readLine()!.split(separator: " ").map{Int(String($0))!}
+//let n = nm[0]
+//let m = nm[1]
+//
+//var arr = readLine()!.split(separator: " ").map{Int(String($0))!}
+//arr.sort()
+//var depth = 0
+//var resultStr = ""
+//var result: [String] = []
+//
+//func dfs(_ depth: Int){
+//    if depth == m {
+//        resultStr += result.joined(separator: " ") + "\n"
+//        return
+//    }
+//    for i in 0..<n{
+//        result.append(String(arr[i]))
+//        dfs(depth + 1)
+//        result.removeLast()
+//    }
+//
+//}
+//
+//dfs(depth)
+//print(resultStr)
