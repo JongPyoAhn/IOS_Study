8d8b5d6b936c6bf29bea67ed0fb9957f25d53ed9
JongPyoAhn <68585628+JongPyoAhn@users.noreply.github.com>
2021-10-04 12:00:59 +0900

    add BOJ

--- Algorithm/CodingTest_Basic/bruteForce/bruteForce/BOJ_10971(원판원순회2).swift
+++ Algorithm/CodingTest_Basic/bruteForce/bruteForce/BOJ_10971(원판원순회2).swift
@@ -7,39 +7,36 @@
 
 //let n = Int(String(readLine()!))!
 //var w: [[Int]] = Array(repeating: [], count: n)
-//var depth = 0
-//var result = Int.max
-//var count = 0
-//var sum = 0
-//
-//
-//var visited: [Bool] = Array(repeating: false, count: n)
 //for i in 0..<n{
 //    let input = readLine()!.split(separator: " ").map{Int(String($0))!}
 //    w[i] = input
 //}
 //
+//var depth = 0
+//var result = Int.max
+//var sum = 0
 //
-//func dfs(_ depth: Int, _ now: Int, _ start: Int, _ answer: Int) {
+//
+//var visited: [Bool] = Array(repeating: false, count: n)
+//
+//
+//func dfs(_ depth: Int, _ now: Int, _ start: Int) {
 //    if depth == n && now == start {
-//        result = min(result, answer)
+//        result = min(result, sum)
 //        return
 //    }
 //    for i in 0..<n {
 //        if !visited[i] && w[now][i] > 0{
 //            visited[i] = true
 //            sum += w[now][i]
-//            if sum <= result{
-//                dfs(depth + 1, i, start, sum)
-//            }
+//                dfs(depth + 1, i, start)
 //            visited[i] = false
 //            sum -= w[now][i]
 //        }
 //    }
 //}
 //
-//for i in 0..<n {
-//    dfs(0, i, i, 0)
-//}
+//dfs(0, 0, 0)
 //
 //print(result)
+//
