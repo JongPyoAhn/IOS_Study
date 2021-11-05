59ea655d78bb52ce6141b50e1007093d6609d31a
JongPyoAhn <68585628+JongPyoAhn@users.noreply.github.com>
2021-11-05 17:16:29 +0900

    add BOJ

---
+++ Algorithm/CodingTest_Basic/DP/DP/BOJ_11057(오르막 수).swift
@@ -0,0 +1,27 @@
+//
+//  main.swift
+//  DP
+//
+//  Created by 안종표 on 2021/11/05.
+//
+
+
+//let n = Int(String(readLine()!))!
+//var dp: [[Int]] = Array(repeating: Array(repeating: 0, count: 10), count: 1001)
+//
+//for i in 0...9 {
+//    dp[1][i] = 1
+//}
+//
+//for i in stride(from: 2, through: n, by: 1){
+//    for j in 0...9 {
+//        for k in 0...j{
+//            dp[i][j] += dp[i - 1][k] % 10007
+//        }
+//    }
+//}
+//var sum = 0
+//for i in 0...9{
+//    sum += dp[n][i]
+//}
+//print(sum % 10007)
