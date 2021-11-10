e8027398e2deed1b328dc2c19c6d3a9099b8e000
JongPyoAhn <68585628+JongPyoAhn@users.noreply.github.com>
2021-11-10 17:41:48 +0900

    add BOJ

---
+++ Algorithm/CodingTest_Basic/DP/DP/BOJ_11722(가장 긴 감소하는 부분수열).swift
@@ -0,0 +1,19 @@
+//
+//  main.swift
+//  DP
+//
+//  Created by 안종표 on 2021/11/10.
+//
+//let a = Int(String(readLine()!))!
+//let arr = readLine()!.split(separator: " ").map{Int(String($0))!}
+//var dp = Array(repeating: 1, count: 1001)
+//
+//for i in 1...a {
+//    for j in 1..<i {
+//        if arr[i - 1] < arr[j - 1]{
+//            dp[i] = max(dp[i], dp[j] + 1)
+//        }
+//    }
+//    
+//}
+//print(dp.max()!)
