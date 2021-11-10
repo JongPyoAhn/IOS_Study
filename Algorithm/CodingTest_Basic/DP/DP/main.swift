192e2eb1e7c31d80cbe5c51b7ae6d13639cb2c83
JongPyoAhn <68585628+JongPyoAhn@users.noreply.github.com>
2021-11-10 13:50:18 +0900

    add BOJ

---
+++ Algorithm/CodingTest_Basic/DP/DP/main.swift
@@ -0,0 +1,21 @@
+//
+//  main.swift
+//  DP
+//
+//  Created by 안종표 on 2021/11/10.
+//
+
+//import Foundation
+//
+//let a = Int(String(readLine()!))!
+//let arr = readLine()!.split(separator: " ").map{Int(String($0))!}
+//var dp = Array(repeating: 0, count: 1001)
+//for i in stride(from: 1, through: a, by: 1){
+//    dp[i] = arr[i - 1]
+//    for j in 1...i{
+//        if arr[i - 1] > arr[j - 1] {
+//            dp[i] = max(dp[j] + arr[i - 1], dp[i])
+//        }
+//    }
+//}
+//print(dp.max()!)
