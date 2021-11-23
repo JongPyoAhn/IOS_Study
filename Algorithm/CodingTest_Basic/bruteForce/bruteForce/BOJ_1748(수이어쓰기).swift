820168a9316e0b954a01a66d799d3e13f3ae8e94
JongPyoAhn <whdvy95@gmail.com>
2021-11-23 14:04:02 +0900

    add

---
+++ Algorithm/CodingTest_Basic/bruteForce/bruteForce/BOJ_1748(수이어쓰기).swift
@@ -0,0 +1,22 @@
+//
+//  main.swift
+//  bruteForce
+//
+//  Created by 안종표 on 2021/11/23.
+//
+
+//import Foundation
+
+//1부터 n까지 수를 이어서 쓴것의 자릿수를 구하자.
+//let n = Int(String(readLine()!))!
+//var count = 0
+//var i = 1
+//while i <= n {
+//    count += n - i + 1
+//    i = i * 10
+//}
+//print(count)
+
+
+
+
