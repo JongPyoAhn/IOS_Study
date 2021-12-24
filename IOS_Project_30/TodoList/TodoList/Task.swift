61c3c5ce7687e7c5315bf1ca73b3e6c65a433c33
JongPyoAhn <whdvy95@gmail.com>
2021-12-24 14:28:00 +0900

    add Toto

---
+++ IOS_Project_30/TodoList/TodoList/Task.swift
@@ -0,0 +1,13 @@
+//
+//  Task.swift
+//  TodoList
+//
+//  Created by 안종표 on 2021/12/24.
+//
+
+import UIKit
+
+struct Task {
+    var title: String
+    var done: Bool
+}
---
+++ IOS_Project_30/TodoList/TodoList/ViewController.swift
@@ -0,0 +1,128 @@
+//
+//  ViewController.swift
+//  TodoList
+//
+//  Created by 안종표 on 2021/12/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet var editButton: UIBarButtonItem!
+    var tasks = [Task]() {
+        //이렇게 되면 tasks배열에 할일이 추가될 때 마다 UserDefaults에 할일이 저장되게된다.
+        didSet {
+            self.saveTasks()
+        }
+    }
+    var doneButton: UIBarButtonItem?
+    
+    @IBOutlet weak var tableView: UITableView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        //done버튼 생성
+        self.doneButton = UIBarButtonItem(barButtonSystemItem: .done, target: self, action: #selector(doneButtonTap))
+        self.tableView.dataSource = self
+        self.tableView.delegate = self
+        self.loadTasks()
+        
+    }
+    @objc func doneButtonTap(){
+        self.navigationItem.leftBarButtonItem = self.editButton
+        self.tableView.setEditing(false, animated: true)
+    }
+    
+    
+    @IBAction func tapEditButton(_ sender: Any) {
+        guard !self.tasks.isEmpty else {return}
+        self.navigationItem.leftBarButtonItem = self.doneButton
+        //테이블뷰 편집모드로
+        self.tableView.setEditing(true, animated: true)
+        
+    }
+    
+    @IBAction func tapAddButton(_ sender: Any) {
+        let alert = UIAlertController(title: "할일등록", message: nil, preferredStyle: .alert)
+        let registerButton = UIAlertAction(title: "등록", style: .default, handler: { [weak self] _ in
+            guard let title = alert.textFields?[0].text else {return}
+            let task = Task(title: title, done: false)
+            self?.tasks.append(task)
+            self?.tableView.reloadData()
+            
+        })
+        let cancelButton = UIAlertAction(title: "취소", style: .cancel, handler: nil)
+        alert.addAction(cancelButton)
+        alert.addAction(registerButton)
+        alert.addTextField(configurationHandler: {textField in
+            textField.placeholder = "할 일을 입력해주세요."
+        })
+        self.present(alert, animated: true, completion: nil)
+    }
+    
+    func saveTasks(){
+        let data = self.tasks.map{
+            [
+                "title" : $0.title,
+                "done" : $0.done
+            ]
+        }
+        let userDefaults = UserDefaults.standard.set(data, forKey: "tasks")
+    }
+    
+    func loadTasks(){
+        let userDefaults = UserDefaults.standard
+        guard let data = userDefaults.object(forKey: "tasks") as? [[String:Any]] else {return}
+        self.tasks = data.compactMap{
+            guard let title = $0["title"] as? String else {return nil}
+            guard let done = $0["done"] as? Bool else {return nil}
+            return Task(title: title, done: done)
+        }
+    }
+}
+
+
+extension ViewController: UITableViewDataSource{
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return tasks.count
+    }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        let task = self.tasks[indexPath.row]
+        cell.textLabel?.text = task.title
+        if task.done{
+            cell.accessoryType = .checkmark
+        }else{
+            cell.accessoryType = .none
+        }
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        self.tasks.remove(at: indexPath.row)
+        
+        tableView.deleteRows(at: [indexPath], with: .automatic)
+        if self.tasks.isEmpty{
+            self.doneButtonTap()
+        }
+    }
+    //행이 다른위치로 이동하면 sourceIndexPath파라미터를 통해 원래 위치를 알려주고
+    //destinationIndexpath를 통해 어디로 이동햇는지 알려줌
+    func tableView(_ tableView: UITableView, moveRowAt sourceIndexPath: IndexPath, to destinationIndexPath: IndexPath) {
+        
+        var tasks = self.tasks
+        let task = tasks[sourceIndexPath.row]
+        tasks.remove(at: sourceIndexPath.row)
+        tasks.insert(task, at: destinationIndexPath.row)
+        self.tasks = tasks
+    }
+}
+
+extension ViewController: UITableViewDelegate{
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        var task = self.tasks[indexPath.row]
+        task.done = !task.done
+        self.tasks[indexPath.row] = task
+        //선택된 셀만 리로드
+        self.tableView.reloadRows(at: [indexPath], with: .automatic)
+    }
+}
