22648ac267264d3f3aeeb2eced7953a05ebd74cb
JongPyoAhn <whdvy95@gmail.com>
2021-12-28 11:42:32 +0900

    add BOJ

---
+++ Algorithm/CodingTest_Practice/BruteForce/BOJ_2003(수들의 합2).swift
@@ -0,0 +1,24 @@
+//
+//  수들의 합2.swift
+//  CodingTest_Practice
+//
+//  Created by 안종표 on 2021/12/28.
+// O
+
+//import Foundation
+//let nm = readLine()!.split(separator: " ").map{Int(String($0))!}
+//let n = nm[0]
+//let m = nm[1]
+//let arr = readLine()!.split(separator: " ").map{Int(String($0))!}
+//
+//var count = 0
+//for i in 0..<n{
+//    var sum = 0
+//    for j in i..<n{
+//        sum += arr[j]
+//        if sum == m{
+//            count += 1
+//        }
+//    }
+//}
+//print(count)
