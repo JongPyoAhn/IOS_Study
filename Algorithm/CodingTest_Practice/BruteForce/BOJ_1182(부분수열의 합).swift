a2558cd492bcada2c831176d8e7ba78083b01cdd
JongPyoAhn <whdvy95@gmail.com>
2022-01-11 16:03:53 +0900

    add BOJ

--- Algorithm/CodingTest_Practice/BruteForce/BOJ_1182(부분수열의 합).swift
+++ Algorithm/CodingTest_Practice/BruteForce/BOJ_1182(부분수열의 합).swift
@@ -5,54 +5,30 @@
 //  Created by 안종표 on 2021/12/13.
 // 16%에서 틀림
 
-//import Foundation
-//
 //let ns = readLine()!.split(separator: " ").map{Int(String($0))!}
 //let n = ns[0]
 //let s = ns[1]
 //
 //let arr = readLine()!.split(separator: " ").map{Int(String($0))!}
-//var countDepth = 1
 //var visited = Array(repeating: false, count: 21)
-//var result = [Int]()
-//var count = 0
+//
+//var cnt = 0
+//var sum = 0
 //func dfs(_ depth: Int, _ start: Int){
-//    if depth == countDepth{
-//        var sum = 0
-//        for i in result{
-//            sum += i
-//        }
-//        if sum == s {
-//            count += 1
-//        }
-//        print(result)
-//        return
+//    if sum == s && depth >= 1{
+//        cnt += 1
 //    }
-//    if start + 1 < n{
-//        while countDepth < n {
-//            for j in (start + 1)..<n{
-//                if !visited[j]{
-//                    visited[j] = true
-//                    result.append(arr[j])
-//                    dfs(depth + 1, start)
-//                    visited[j] = false
-//                    result.removeLast()
-//                }
-//            }
-//            countDepth += 1
+//    
+//    for i in start..<n{
+//        if !visited[i]{
+//            visited[i] = true
+//            sum += arr[i]
+//            dfs(depth + 1, i)
+//            visited[i] = false
+//            sum -= arr[i]
 //        }
-//
 //    }
 //}
-//for i in 0..<n{
-//    countDepth = 1
-//    visited[i] = true
-//    result.append(arr[i])
 //
-//    if arr[i] == s {
-//        count += 1
-//    }
-//    dfs(0, i)
-//    result.removeAll()
-//}
-//print(count)
+//dfs(0, 0)
+//print(cnt)
