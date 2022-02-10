a4f0a21911adce0c6eea292c3ce7226ae0d95f8e
JongPyoAhn <whdvy95@gmail.com>
2022-02-10 09:36:50 +0900

    add BOj

--- Algorithm/CodingTest_Practice/BFS/BOJ_16928(뱀과 사다리 게임).swift
+++ Algorithm/CodingTest_Practice/BFS/BOJ_16928(뱀과 사다리 게임).swift
@@ -3,64 +3,47 @@
 //  CodingTest_Practice
 //
 //  Created by 안종표 on 2021/12/13.
-// 다시
+// O
 
-//import Foundation
 //let nm = readLine()!.split(separator: " ").map{Int(String($0))!}
-//let n = nm[0] //사다리의 수
-//let m = nm[1] // 뱀의 수
+//let n = nm[0]
+//let m = nm[1]
 //
-//var nArr = [[Int]]()
-//var mArr = [[Int]]()
-//var queue = [(Int, Int)]()
-//var visited = Array(repeating: false, count: 101)
-//var isChecked = true
-//for _ in 0..<n{
-//    nArr.append(readLine()!.split(separator: " ").map{Int(String($0))!})
+//var arr = [[Int]]()
+//for _ in 0..<n + m{
+//    arr.append(readLine()!.split(separator: " ").map{Int(String($0))!})
 //}
 //
-//for _ in 0..<m{
-//    mArr.append(readLine()!.split(separator: " ").map{Int(String($0))!})
-//}
 //
 //func bfs(){
-//    queue.append((1,0))
+//    var visited = Array(repeating: false, count: 101)
+//    var queue = [(Int, Int)]()//현재위치, 주사위굴린 횟수
+//        
+//    queue.append((1, 0))
 //    visited[1] = true
-//    while !queue.isEmpty{
-//        let pop = queue.removeFirst()
-//        if pop.0 == 100{
+//    var idx = 0
+//    while queue.count > idx{
+//        let pop = queue[idx]
+//        idx += 1
+//        var curLocation = pop.0
+//        
+//        if curLocation == 100{
 //            print(pop.1)
 //            break
 //        }
-//
-//
-//        for i in 1...6{
-//            if pop.0 + i <= 100 && !visited[pop.0 + i] {
-//                for j in 0..<n{
-//                    if nArr[j][0] == pop.0 + i{
-//                        queue.append((nArr[j][1], pop.1 + 1))
-//                        visited[nArr[j][1]] = true
-//                        isChecked = false
-//                    }
-//                }
-//                for j in 0..<m{
-//                    if mArr[j][0] == pop.0 + i{
-//                        queue.append((mArr[j][1], pop.1 + 1))
-//                        visited[mArr[j][1]] = true
-//                        isChecked = false
-//                    }
-//                }
-//
-//                if isChecked{
-//                    queue.append((pop.0 + i, pop.1 + 1))
-//                    visited[pop.0 + i] = true
-//                }
+//        for i in arr{
+//            if i[0] == pop.0{
+//                curLocation = i[1]
 //            }
-//            isChecked = true
 //        }
-//
+//        for i in 1...6{
+//            let move = curLocation + i
+//            if move > 100 || visited[move] {continue}
+//            queue.append((move, pop.1 + 1))
+//            visited[move] = true
+//        }
+//        
 //    }
 //}
-//
+//                    
 //bfs()
-
---
+++ Algorithm/CodingTest_Practice/DP/main.swift
@@ -0,0 +1,6 @@
+//
+//  main.swift
+//  CodingTest_Practice
+//
+//  Created by 안종표 on 2022/02/08.
+//
