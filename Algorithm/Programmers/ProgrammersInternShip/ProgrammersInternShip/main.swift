8f69c7c202ba6a663d518e8e2cb335d77733148b
JongPyoAhn <whdvy95@gmail.com>
2022-02-22 16:37:47 +0900

    add

--- Algorithm/Programmers/ProgrammersInternShip/ProgrammersInternShip/main.swift
+++
@@ -1,8 +0,0 @@
-//
-//  main.swift
-//  ProgrammersInternShip
-//
-//  Created by 안종표 on 2021/11/19.
-//
-
-import Foundation
---
+++ Algorithm/Programmers/고득점Kit/위장(해시).swift
@@ -0,0 +1,27 @@
+//
+//  main.swift
+//  Programmers
+//
+//  Created by 안종표 on 2022/02/22.
+//
+
+//import Foundation
+//print(solution([["crowmask", "face"], ["bluesunglasses", "face"], ["smoky_makeup", "face"]]))
+//func solution(_ clothes:[[String]]) -> Int {
+//    var result = 1
+//    var dict = [String: Int]()
+//    for i in clothes{
+//        if dict[i[1]] != nil {
+//            dict[i[1]]! += 1
+//        }else{
+//            dict[i[1]] = 2
+//        }
+//    }
+//
+//    for v in dict.values{
+//        result *= v
+//    }
+//    result -= 1
+//
+//    return result
+//}
