556f73eb94c0641d01de57d01fc13329427117de
JongPyoAhn <whdvy95@gmail.com>
2022-03-02 12:31:09 +0900

    add

---
+++ Algorithm/CodingTest_Practice/BFS/main.swift
@@ -0,0 +1,7 @@
+//
+//  main.swift
+//  CodingTest_Practice
+//
+//  Created by 안종표 on 2022/03/02.
+//
+
---
+++ Algorithm/CodingTest_Practice/DP/BOJ_11058(크리보드).swift
@@ -0,0 +1,22 @@
+//
+//  main.swift
+//  CodingTest_Practice
+//
+//  Created by 안종표 on 2022/02/28.
+// 다시
+
+
+//let n = Int(String(readLine()!))!
+//var dp = Array(repeating: 0, count: 101)
+//for i in 1...n{
+//    if i <= 6 {
+//        dp[i] = i
+//    }else{
+//        for j in stride(from: 3, through: n, by: 1){
+//            if i - j <= 0 {continue}
+//            dp[i] = max(dp[i], dp[i - j] * (j - 1))
+//        }
+//    }
+//    
+//}
+//print(dp[n])
