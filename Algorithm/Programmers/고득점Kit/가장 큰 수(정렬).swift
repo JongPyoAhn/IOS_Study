f38e729e7ee59dc93316a706643b3b9c206e08ae
JongPyoAhn <whdvy95@gmail.com>
2022-04-13 10:18:02 +0900

    Programmers

--- Algorithm/Programmers/고득점Kit/가장 큰 수(정렬).swift
+++ Algorithm/Programmers/고득점Kit/가장 큰 수(정렬).swift
@@ -3,22 +3,42 @@
 //  Programmers
 //
 //  Created by 안종표 on 2022/04/13.
-//
-import Foundation
-print(solution([1, 5, 2, 6, 3, 7, 4], [[2, 5, 3], [4, 4, 1], [1, 7, 3]]))
-func solution(_ array:[Int], _ commands:[[Int]]) -> [Int] {
-    var result = [Int]()
-    for v in 0..<commands.count{
-        let i = commands[v][0] - 1
-        let j = commands[v][1] - 1
-        let k = commands[v][2] - 1
-        
-        var arr = Array(array[i...j])
-        arr.sort()
-        result.append(arr[k])
-        
-    }
-    
-    
-    return result
-}
+// 다시
+
+//import Foundation
+////[101, 900]
+////[990, 99]
+//print(solution([0, 0]))
+
+//func solution(_ numbers:[Int]) -> String {
+//    var check = false
+//    var result = ""
+//    var tempNumbers = numbers
+//    tempNumbers.sort{
+//        "\($0)" + "\($1)" > "\($1)" + "\($0)"
+//    }
+//    for i in tempNumbers{
+//        if i != 0 && !check {
+//            check = true
+//            result = ""
+//        }
+//        if check{
+//            result += "\(i)"
+//        }else{
+//            result = "0"
+//        }
+//    }
+//    return result
+//}
+
+//func solution(_ numbers:[Int]) -> String {
+//    var result = ""
+//    var tempNumbers = numbers
+//    tempNumbers.sort{
+//        "\($0)" + "\($1)" > "\($1)" + "\($0)"
+//    }
+//    for i in tempNumbers{
+//        result += "\(i)"
+//    }
+//    return String(Int(result)!)
+//}
