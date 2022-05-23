5e84185fa251e0e9393fd1695c674cb8124bdeba
JongPyoAhn <whdvy95@gmail.com>
2022-05-23 14:35:24 +0900

    add

---
+++ Algorithm/CodingTest_Practice/이분탐색/BOJ_1654(랜선자르기).swift
@@ -0,0 +1,32 @@
+//
+//  main.swift
+//  CodingTest_Practice
+//
+//  Created by 안종표 on 2022/05/23.
+//
+
+//let kn = readLine()!.split(separator: " ").map{Int(String($0))!}
+//let k = kn[0]
+//let n = kn[1]
+//var arr = [Int]()
+//for _ in 0..<k{
+//    arr.append(Int(String(readLine()!))!)
+//}
+//
+//let maxArr = arr.max()!
+//var start = 1
+//var end = maxArr
+//
+//while start <= end{
+//    var count = 0
+//    let mid = (start + end) / 2
+//    for i in 0..<k{
+//        count += (arr[i] / mid)
+//    }
+//    if count < n{
+//        end = mid - 1
+//    }else{
+//        start = mid + 1
+//    }
+//}
+//print(start - 1)
